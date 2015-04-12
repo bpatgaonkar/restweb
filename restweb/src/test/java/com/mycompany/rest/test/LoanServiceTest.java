eb10b3af1448175b9f5cde18ed95f2b150857c8d
bpatgaonkar <patgaonkar.bhushan@gmail.com>
2015-04-11 22:24:36 -0700

    updated correct package

--- restweb/src/test/java/com/mycompany/rest/test/LoanServiceTest.java
+++ restweb/src/test/java/com/mycompany/rest/test/LoanServiceTest.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.dorado.rest.test;
+package com.mycompany.rest.test;
 
 import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.ClientResponse;
